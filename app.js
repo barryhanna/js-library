159c95112644819e84ad39d8f4b5b4d747d34479
Barry Hanna <barryhanna@users.noreply.github.com>
2022-08-27 11:26:47 +0100

    Added new book function

--- app.js
+++ app.js
@@ -1,19 +1,31 @@
-let myLibrary = [
-  { title: 'Book 1', author: 'Some Author', pages: 75, read: true },
-  { title: 'Book 2', author: 'Author', pages: 25, read: false },
-  { title: 'Book 3', author: 'Author 2', pages: 353, read: true },
-  { title: 'Book 4', author: 'Someone', pages: 123, read: false },
-];
+// TODO: Bug fix - when table is removed the 'New Book' button doesn't work
 
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
+
+  this.toggleRead = () => (this.read = !this.read);
 }
+
+let myLibrary = [
+  new Book('Book 1', 'Some Author', 75, true),
+  new Book('Book 2', 'Author', 25, false),
+  new Book('Book 3', 'Author 2', 353, true),
+  new Book('Book 4', 'Someone', 123, false),
+];
+
+const form = document.querySelector('form');
+const showFormBtn = document.querySelector('.show-form');
 
 function addBookToLibrary(book) {
   myLibrary.push(book);
+}
+
+function toggleBookRead(id) {
+  myLibrary[id].toggleRead();
+  showBooks();
 }
 
 function showBooks() {
@@ -24,14 +36,21 @@ function showBooks() {
   table.innerHTML = '';
   const main = document.querySelector('main');
 
-  table.innerHTML += `<tr><th>id</th><th>Title</th><th>Author</th><th>Pages</th><th>Read?</th></tr>`;
+  table.innerHTML += `<thead><tr style="text-align: left;"><th hidden>id</th><th>Title</th><th>Author</th><th>Pages</th><th>Read?</th></tr></thead><tdbody>`;
   let bookRows = '';
-  myLibrary.forEach(
-    (book, index) =>
-      (bookRows += `<tr><td>${index}</td><td>${book.title}</td><td>${book.author}</td><td>${book.pages}</td><td>${book.read}</td><td><button onclick='deleteBookFromLibrary(${index})'>X</button></td></tr>`)
-  );
-  table.innerHTML += bookRows;
-  main.appendChild(table);
+  if (myLibrary.length > 0) {
+    myLibrary.forEach(
+      (book, index) =>
+        (bookRows += `<tr><td hidden>${index}</td><td>${book.title}</td><td>${book.author}</td><td style="text-align: right;">${book.pages}</td><td onclick='toggleBookRead(${index})'>${book.read}</td><td><button onclick='deleteBookFromLibrary(${index})'>x</button></td></tr>`)
+    );
+    bookRows += '</body>';
+    table.innerHTML += bookRows;
+    main.appendChild(table);
+  } else {
+    document.querySelector('table').remove();
+    main.innerHTML +=
+      "There are no books in your library. Click 'New Book' to add one";
+  }
 }
 
 function deleteBookFromLibrary(id) {
@@ -39,19 +58,20 @@ function deleteBookFromLibrary(id) {
   showBooks();
 }
 
-const form = document.querySelector('form');
-form.style.visibility = 'hidden';
+const hideForm = () => {
+  form.style.visibility = 'hidden';
+  showFormBtn.textContent = 'New Book';
+};
 
-const showFormBtn = document.querySelector('.show-form');
+const showForm = () => {
+  form.style.visibility = 'visible';
+  showFormBtn.textContent = 'Cancel';
+};
+
+hideForm();
 
 showFormBtn.addEventListener('click', (e) => {
-  if (form.style.visibility === 'hidden') {
-    form.style.visibility = 'visible';
-    showFormBtn.textContent = 'Cancel';
-  } else {
-    form.style.visibility = 'hidden';
-    showFormBtn.textContent = 'New Book';
-  }
+  form.style.visibility === 'hidden' ? showForm() : hideForm();
 });
 
 document.querySelector('.add-book-btn').addEventListener('click', (e) => {
@@ -64,8 +84,13 @@ document.querySelector('.add-book-btn').addEventListener('click', (e) => {
   addBookToLibrary(newBook);
   clearForm();
   showBooks();
+  hideForm();
 });
 
 function clearForm() {
   document.getElementsByTagName('form')[0].reset();
+}
+
+if (myLibrary.length > 0) {
+  showBooks();
 }
